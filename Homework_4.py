b98ce20134510b259571b11ab3d377ab1aa4ba3d
AlekseiKustov <69516948+AlekseiKustov@users.noreply.github.com>
2020-09-14 14:43:50 +0300

    Update Homework_4.py

--- Homework_4.py
+++ Homework_4.py
@@ -59,6 +59,65 @@ print(f'Произведение всех элементов списка {reduc
 
 
 
+#6. Реализовать два небольших скрипта:
+#а) итератор, генерирующий целые числа, начиная с указанного,
+#б) итератор, повторяющий элементы некоторого списка, определенного заранее.
+#Подсказка: использовать функцию count() и cycle() модуля itertools.
+# Обратите внимание, что создаваемый цикл не должен быть бесконечным.
+# Необходимо предусмотреть условие его завершения.
+#Например, в первом задании выводим целые числа, начиная с 3, а при достижении числа 10 завершаем цикл.
+#Во втором также необходимо предусмотреть условие, при котором повторение элементов списка будет прекращено.#
+
+
+
+from itertools import count, cycle
+
+value = 10
+generator_a = (item for i, item in enumerate(count(value)) if i < boundary)
+
+def number_int(first_number, last_number):
+    for num in count(first_number):
+        if num > last_number:
+            break
+        else:
+            yield num
+
+
+def doublikate_list(any_list):
+    iteration_count = 0
+    for elem in cycle(any_list):
+        if iteration_count > (len(any_list) - 1):
+            break
+        yield elem
+        iteration_count += 1
+
+if __name__ == "__main__":
+    example = list(number_int(5, 20))
+    print(example)
+    copy_example = list(doublikate_list(example))
+    print(copy_example)
+#7. Реализовать генератор с помощью функции с ключевым словом yield, создающим очередное значение.
+# При вызове функции должен создаваться объект-генератор.
+# Функция должна вызываться следующим образом: for el in fact(n).
+# Функция отвечает за получение факториала числа, а в цикле необходимо выводить только первые n чисел, начиная с 1! и до n!.
+#Подсказка: факториал числа n — произведение чисел от 1 до n. Например, факториал четырёх 4! = 1 * 2 * 3 * 4 = 24.
+
+
+from itertools import count
+from math import factorial
+
+def fibo_gen():
+    for el in count(1):
+        yield factorial(el)
+
+gen = fibo_gen()
+x = 0
+for i in gen:
+    if x < 15:
+        print(i)
+        x += 1
+    else:
+        break
 
 
 
